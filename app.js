9e2ec07ffcff8c26e4e09d5b402701b6652c91c3
Umidbek Xajiboyev <diyorsaratonov208@gmail.com>
2024-05-17 21:59:05 +0500

    2-dars javascript darsligi yangilandi

---
+++ app.js
@@ -0,0 +1,15 @@
+// Variables - qiymatlar saqlash uchun kerak;
+let name = 'James';
+let age = 16;
+
+console.log(name, age);
+age = 17;
+
+console.log(age);
+
+
+
+// const ozgarmas
+
+const place = 'Xanqa';
+console.log(place);
