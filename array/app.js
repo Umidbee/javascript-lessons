f87910309390d79b8dd137a82098696083805c98
Umidbek Xajiboyev <diyorsaratonov208@gmail.com>
2024-05-18 17:10:58 +0500

    yangilanish kiritildi

---
+++ array/app.js
@@ -0,0 +1,40 @@
+// const mexmon1 = 'Salim'
+// const mexmon2 = 'Umidbek'
+// const mexmon3 = 'Boburbek'
+// array => massive
+// const mehmonlar = ['Salim', 'Umidbek', 'Boburbek', 33, 11];
+// console.log(mehmonlar);
+
+// mehmonlar[0] = 'Ahror'
+// console.log(mehmonlar);
+
+// let number = [1, 2, 3, 4,]
+
+//property
+
+// const result = number.length;
+// console.log(result);
+
+// indexOf array ichidagi number, va boshqa narsalarni pazitsiyasini aniqlab beradi
+
+//  concat properti vazifasi array ichidagi narsalarga qoshimcha malumot qoshib beradi 
+
+// mehmonlar.push('Davronbek')
+
+//pop array ichidan malumot olib tashlaydi
+// mehmonlar.pop('Salim')
+// console.log(mehmonlar);
+// let result = mehmonlar.concat(number)
+// console.log(result);
+
+
+
+
+
+const mexmonlar = ['Umidbek', 'Jasurbek', 'Jamshid', 'Bexruz', 22, 22];
+console.log(mexmonlar);
+
+// const result = mexmonlar.length;
+// console.log(result);
+
+
